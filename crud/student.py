5c0ee91e412235f24670197b52e89fc8e1e0a23a
Gavhar <saraah5778@gmail.com>
2025-05-03 17:43:43 +0500

    added

---
+++ crud/student.py
@@ -0,0 +1,11 @@
+from tokenize import group
+
+
+class student():
+    def __init__(self):
+        self.fullname = fullname
+        self.group = group
+        self.gender=gender
+        self.phone_number= phone_number
+
+
---
+++ crud/teacher.py
@@ -0,0 +1,5 @@
+class teacher :
+    def __init__(self):
+        self.teacher_name=teacher_name
+        self.teacher_classes= []
+        self.teacher_subject=teacher_subject
